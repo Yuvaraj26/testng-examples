494da9273ce3c59365a098958867cf1ce34df1da
Yuvaraj26 <yuvi1064@gmail.com>
2020-02-26 23:45:48 +0530

    Testng

---
+++ Testngexample/src/testpackage/Basic.java
@@ -0,0 +1,12 @@
+package testpackage;
+
+import org.testng.annotations.Test;
+
+public class Basic {
+	@Test
+	public void demo()
+	{
+		System.out.println("hello");
+	}
+
+}
---
+++ Testngexample/src/testpackage/xmlfile.java
@@ -0,0 +1,19 @@
+package testpackage;
+
+import org.testng.annotations.Test;
+
+public class xmlfile {
+	
+	@Test
+	public void demo()
+	{
+		System.out.println("hello");
+	}
+	@Test
+	public void seconddemo()
+	{
+		System.out.println("world");
+	}
+	
+
+}
