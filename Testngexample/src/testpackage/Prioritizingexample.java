e81b3637851c130312b53ba667e5304631212e55
Yuvaraj26 <yuvi1064@gmail.com>
2020-02-27 23:47:26 +0530

    Changes

--- Testngexample/src/testpackage/Prioritizingexample.java
+++ Testngexample/src/testpackage/Prioritizingexample.java
@@ -1,5 +1,37 @@
 package testpackage;
 
+import org.testng.annotations.Test;
+
 public class Prioritizingexample {
+	
+	@Test
+	public void  webloginloan()
+	{
+		//selenium
+		System.out.println("webloginloan");
+	}
+	@Test
+	public void mobileloginloan()
+	{
+		System.out.println("mobileloginloan");
+	}
+	
+	@Test
+	public void mobileloginloan1()
+	{
+		System.out.println("mobileloginloan");
+	}
+	
+	@Test
+	public void mobileloginloan2()
+	{
+		System.out.println("mobileloginloan");
+	}
+	
+	@Test
+	public void APIlogin()
+	{
+		System.out.println("APIlogin");
+	}
 
 }
---
+++ Testngexample/src/testpackage/Prioritizingexample2.java
@@ -0,0 +1,25 @@
+package testpackage;
+
+import org.testng.annotations.Test;
+
+public class Prioritizingexample2 {
+	
+	@Test
+	public void  webloginhomeloan()
+	{
+		//selenium
+		System.out.println("webloginloan");
+	}
+	@Test
+	public void mobileloginomeloan()
+	{
+		System.out.println("mobileloginloan");
+	}
+	
+	@Test
+	public void APIhomelogin()
+	{
+		System.out.println("APIlogin");
+	}
+
+}
